576a8516fc0ad2e93e4f8be13bb9f97616944995
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-05-31 20:41:40 +0800

    add : expand,bestchild, getters

--- Batch/Mac/Sourcecode/UCT.h
+++ Batch/Mac/Sourcecode/UCT.h
@@ -12,6 +12,7 @@
 #define C (sqrt(2)) // define constant whatever you want 
 using NodePosi = Node*;
 struct Node{
+private:
     /*------------MEMBERS--------*/
     int ** board_;
     double value;
@@ -20,17 +21,19 @@ struct Node{
     int * top_;
     bool isleaf;
     int n;            // number of visiting
-    int w;            // number of win
-    int lastx,lasty; // last position that opponent put
+    int r;            // result of the node
+    int lastx,lasty;  // last position that opponent put (not used)
+    int x,y;
     int no_x,no_y;    // banned position
-    int expandables;
     NodePosi parent;
     NodePosi * children;
-    int* expandable_nodes;
+    int expandables;        // for expand, also can track how many times this node has been expanded
+    int* expandable_nodes;  // for expand
     /*----------GENERATORS & ------------*/
+public:
     Node() {}
     Node(int ** board,const int M, const int N, int* top,const int noX, const int noY, bool is_leaf = true)
-    : value(0.0),row(M), column(N),isleaf(is_leaf), n(0),w(0)
+    : value(0.0),row(M), column(N),isleaf(is_leaf), n(0),r(0)
         ,lastx(-1),lasty(-1),no_x(noX),no_y(noY),expandables(true)
         ,parent(nullptr){
             // copy board
@@ -47,8 +50,8 @@ struct Node{
                 top_[i] = top[i];
             }
 
-            // initiate children list
-            children = new NodePosi[column];
+            // initiate array 'children' with nullptrs
+            children = new NodePosi[column]();
             // create expandable nodes list
             expandable_nodes = new int[N];
 
@@ -74,20 +77,26 @@ struct Node{
     /*-----------INTERFACES-----------*/
     bool& is_leaf() {return isleaf;}
     bool is_expandable() {return expandables > 0;}
-    int& get_n() { return n;}
-    int& get_w() { return w;}
-    void set_value(){value = (double)w / (double)n ;}
-    NodePosi get_parent() { return parent;};
+    //------------ getters-------------
+    const int& get_x() {return x;}
+    const int& get_y() {return y;}
+    int& get_n() {return n;}
+    int& get_r() {return r;}
+    void set_value() {value = (double)r / (double)n ;}
+    double get_value() {return value;}
+    NodePosi get_parent() { return parent;}
+    void set_parent(NodePosi to_be_parent) {parent = to_be_parent;}
 
     /*-----------FUNCTIONS------------*/
     NodePosi tree_policy();
     int rollout();
     NodePosi expand();
     void backup(int delta);
-    Node* bestchild(const double c=C);
+    NodePosi bestchild(const double c=C);
 };
 
 NodePosi Node::tree_policy(){
+    // @todo : 
     NodePosi v = this;
     while(!v->is_leaf()){
         if(v->is_expandable()){
@@ -101,72 +110,61 @@ NodePosi Node::tree_policy(){
 
 int Node::rollout(){
     // @TODO : rollout = default policy
-    // ????????????????????????????????
+    //         simulate game whose initial state is this board
+
 };
 
 NodePosi Node::expand(){
     // @TODO : select yet explored node,
+    /*
+        - only called when 'this' node is expandable 
+        - children array is initialized with nullptr at the first site,
+    */
+    for(int i = 0; i < column; i++){
+        if(top_[i] > 0){
+            if(children[i] == nullptr){
+                // will put on (top_[i]-1,i)
+                board_[top_[i]-1][i] = 2;
+                children[i] = new Node(board_,row,column,top_,no_x,no_y,true);
+                children[i]->set_parent(this);
+                board_[top_[i]-1][i] = 0;
+                return children[i];
+            }
+        }
+    }
 };
 
 void Node::backup(int delta){
     // @TODO : backpropagate till root 
+    //          consider tie!!
     NodePosi v = this;
     while(v){
         v->get_n() += 1;
-        v->get_w() += delta;
+        v->get_r() += delta;
         v->set_value();
-        delta = 1 - delta;
+        delta = -1*delta;
         v = v->get_parent();
     }
-
 };
 
-Node* Node::bestchild(const double c=C){ 
+NodePosi Node::bestchild(const double c=C){ 
     // @TODO : traverse all children of 'this' 
     //          and find maximum value
+    //          b_child won't be null 
     NodePosi b_child{nullptr};
     double res = 0;
-    // for{
-        double value_ = value + C*(log(this->get_parent()->get_n()/get_parent()));
-        res = MAX(res,value_);
-    // }
-
+    for(int i=0; i< column ; i++){
+        if(children[i] == nullptr) continue;
+        NodePosi child = children[i];
+        if(child->get_n() == 0)
+            return child;
+        double value_ = child->get_value() + ((double)C)*(log((double)this->get_n()/(double)child->get_n()));
+        if( res < value_){
+            res = value_;
+            b_child = child;
+        }
+    }
+    return b_child;
 };
 
-/*-------------------------------------------------------*/
-// class Node{
-// public:
-//     // int ** _board;
-//     double win_rate;
-//     int row;          // M
-//     int column;       // N
-//     bool isleaf;
-//     int n;            // number of visiting
-//     int w;            // number of win
-//     int lastx,lasty; // last position that opponent put
-//     int no_x,no_y;    // banned position
-//     bool expandable;
-//     /*------------------------*/
-//     Node * parent{NULL};
-//     Node ** children;
-// // Constructor
-//     Node() : isleaf(true){
-//     }
-//     Node(const int M, const int N, const int noX, const int noY)
-//     : win_rate(0.0),row(M), column(N),isleaf(true), n(0),w(0)
-//         ,lastx(-1),lasty(-1),no_x(noX),no_y(noY),expandable(true){
-//     };
-
-//     Node* tree_policy();
-//     void bestchild();
-// };
-
-// Node* Node::tree_policy(){
-
-// };
-// void Node::bestchild(){
-//     // @TODO : traverse all children of 'this' 
-//     //          and find maximum value
-//     double res = 0;
-// };
 #endif
